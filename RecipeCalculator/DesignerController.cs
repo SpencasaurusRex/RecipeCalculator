cf255d9a9e7f46540531c7171d82cee28e5db8df
Spencer Haney <theOfficialSkara@gmail.com>
2016-10-17 04:27:20 -0400

    Added GUI setup

---
+++ RecipeCalculator/DesignerController.cs
@@ -0,0 +1,19 @@
+﻿
+namespace RecipeCalculator
+{
+    class DesignerController
+    {
+        private DesignerView view;
+
+        public DesignerController(DesignerView view)
+        {
+            this.view = view;
+        }
+
+        public void AttemptExit()
+        {
+            // TODO add check for unsaved files
+            view.Close();
+        }
+    }
+}
---
+++ RecipeCalculator/Startup.cs
@@ -0,0 +1,17 @@
+﻿using RecipeCalculator;
+using System;
+using System.Windows.Forms;
+
+static class Startup
+{
+    /// <summary>
+    /// The main entry point for the application
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new DesignerView());
+    }
+}
